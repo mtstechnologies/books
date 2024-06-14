4dd16ca92fb00ca24d3385e35beee39301e27536
Michael Trindade <michael.trindade@hotmail.com>
2024-06-14 18:27:17 -0400

    Testing First Route

--- books-ui/src/index.js
+++ books-ui/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
 import { createGlobalStyle } from 'styled-components';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const GlobalStyle = createGlobalStyle`
 body {
@@ -27,7 +27,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/favoritos" element={<p>Oi!</p>} />
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
