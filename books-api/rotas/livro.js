9d960edae5c4f1a6ddc6c9391ea60000e53b5697
Michael Trindade <michael.trindade@hotmail.com>
2024-06-13 20:02:31 -0400

    Basic routes created

---
+++ books-api/rotas/livro.js
@@ -0,0 +1,21 @@
+const {Router} = require("express")
+
+const router = Router()
+
+router.get('/', (req, res) => {
+    res.send("Olá mundo da alura!")
+})
+
+router.post('/', (req, res) => {
+    res.send("Voce fez uma requisicao do tipo POST")
+})
+
+router.patch('/', (req, res) => {
+    res.send("Voce fez uma requisicao do tipo PATCH")
+})
+
+router.delete('/', (req, res) => {
+    res.send("Voce fez uma requisicao do tipo DELETE")
+})
+
+module.exports = router
