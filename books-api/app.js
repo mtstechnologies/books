cd43b5322d3f43186164ce1d8bca31aa853ed7d4
Michael Trindade <michael.trindade@hotmail.com>
2024-06-14 23:18:53 -0400

    First frontend request for backend and cors released

--- books-api/app.js
+++ books-api/app.js
@@ -1,10 +1,12 @@
 const express = require("express")
-
 const rotaLivro = require("./rotas/livro")
+const cors = require("cors")
 
 const app = express()
 //para que o express possa receber um body do tipo JSON
 app.use(express.json())
+
+app.use(cors({origin: "*"}))//liberando acesso para qualquer origem
 
 app.use('/livros', rotaLivro)
 
